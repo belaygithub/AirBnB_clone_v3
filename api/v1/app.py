2f283ff67d61785aba58639c63756c693342d00e
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-11-21 21:01:06 +0300

    adavance task done

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
-"""flask"""
-from flask import Flask, render_template, jsonify
-from models import storage
-from api.v1.views import app_views
-from os import getenv
+"""
+Sets up Flask application
 
+"""
+
+
+from os import getenv
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
+
+from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
-host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
-port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else 5000
-
-
-@app.teardown_appcontext
-def teardown_db(self):
-    """teardown"""
-    storage.close()
+CORS(app, orgins='0.0.0.0')
+app.register_blueprint(app_views)
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """404ed"""
-    return jsonify({"error": "Not found"}), 404
+def page_not_found(error):
+    """Returns JSON error repsponse"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Closes storage session"""
+    storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host=host, port=port, threaded=True)
+    api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    api_port = getenv('HBNB_API_PORT', default=5000)
+    app.run(host=api_host, port=int(api_port), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
-"""Route"""
+"""Retrieves number for each type"""
+from flask import jsonify
+from models import storage
 from api.v1.views import app_views
-from flask import jsonify, Blueprint, render_template, abort
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route("/status", strict_slashes=False)
 def status():
-    """status"""
-    return (jsonify({"status": "OK"}))
+    """Returns status"""
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """Returns stats"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
